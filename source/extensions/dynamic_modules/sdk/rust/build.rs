1ac1360e1a24b863b5a32fd29a8a4d08c4336271
Anuraag Agrawal <anuraaga@gmail.com>
2025-12-12 23:35:33 +0900

    Try

--- source/extensions/dynamic_modules/sdk/rust/build.rs
+++ source/extensions/dynamic_modules/sdk/rust/build.rs
@@ -24,8 +24,12 @@ fn main() {
       non_exhaustive: false,
     })
     .parse_callbacks(Box::new(bindgen::CargoCallbacks::new()))
-    .parse_callbacks(Box::new(TrimEnumNameFromVariantName))
-    .generate()
+    .parse_callbacks(Box::new(TrimEnumNameFromVariantName));
+
+    #[cfg(target_os = "windows")]
+    let bindings = bindings.extern_fn_block_attrs("#[link(name = \"envoy\", kind = \"raw-dylib\")]");
+
+    let bindings = bindings.generate()
     .expect("Unable to generate bindings");
 
   let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
