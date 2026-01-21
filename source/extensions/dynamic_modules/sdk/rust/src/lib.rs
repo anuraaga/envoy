dac95fcbd8f6889822461a8db14266fd7129514e
Anuraag Agrawal <anuraaga@gmail.com>
2026-01-21 13:35:02 +0900

    Revert "Remove yolo"
    This reverts commit fb87c86f07c76f2ecb5ca08232a4b7849f2c1a14.

--- source/extensions/dynamic_modules/sdk/rust/src/lib.rs
+++ source/extensions/dynamic_modules/sdk/rust/src/lib.rs
@@ -2449,7 +2449,7 @@ impl EnvoyHttpFilter for EnvoyHttpFilterImpl {
   }
 
   fn get_worker_index(&self) -> u32 {
-    0
+    unsafe { abi::envoy_dynamic_module_callback_http_filter_get_worker_index(self.raw_ptr) }
   }
 
   fn set_socket_option_int(
@@ -4725,7 +4725,7 @@ impl EnvoyNetworkFilter for EnvoyNetworkFilterImpl {
   }
 
   fn get_worker_index(&self) -> u32 {
-    0
+    unsafe { abi::envoy_dynamic_module_callback_network_filter_get_worker_index(self.raw) }
   }
 }
 
